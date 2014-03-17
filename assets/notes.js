b468cfe595fde431c46034711984d3c4e2ba850f
William Langford <langfordwill@sky.com>
2014-03-17 19:04:44 +0000

    1

--- assets/notes.js
+++ assets/notes.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
 	$(".np").css("display", "none");
 	$(".nhd").click(function(){
-		$(".np", this).css("display", "auto");
+		if($(this).css("dispaly") == "auto"){
+			$(".np", this).css("display", "none");
+		}else{
+			$(".np", this).css("display", "auto");
+		}
 	});
 });
