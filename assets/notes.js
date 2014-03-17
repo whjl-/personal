cf3d834ad7d4d8364f8128382a6be00fcb0cf06a
William Langford <langfordwill@sky.com>
2014-03-17 18:28:11 +0000

    1

--- assets/notes.js
+++ assets/notes.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	$(".np").css("display", "none");
 	$(".nh").click(function(){
-		$(this).children().css("display", "auto");
+		$(".np").css("display", "auto");
 	});
 });
