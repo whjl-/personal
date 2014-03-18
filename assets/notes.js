925b4c58e16341870952d160d8e665466a265310
William Langford <langfordwill@sky.com>
2014-03-18 07:33:11 +0000

    1

--- assets/notes.js
+++ assets/notes.js
@@ -7,11 +7,8 @@ $(document).ready(function(){
 		nh = nh.replace('/[^\-\s\pN\pL]+/g', '');
 		nh = nh.replace('/[\-\s]+/g', '-');
 		nh = $.trim(nh);
-		alert(id);
-		alert(nh);
 		if(nh == id){
-			alert("hi");
-			$(this).parent().find('.np').css("display", "auto");
+			$(obj).parent().find('.np').css("display", "auto");
 		}
 	});
 	$(".np").css("display", "none");
