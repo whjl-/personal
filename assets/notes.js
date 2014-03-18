5fd75cb4320023a4ea2d1ec97a8d13e448e0d434
William Langford <langfordwill@sky.com>
2014-03-18 07:29:28 +0000

    1

--- assets/notes.js
+++ assets/notes.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
+	var url = document.URL;
+	var id = url.substring(url.lastIndexOf('#') + 1);
+	var nh = "";
+	$(".nh").each(function(i, obj){
+		nh = $(this).val().toLowerCase();
+		nh = nh.replace('/[^\-\s\pN\pL]+/g', '');
+		nh = nh.replace('/[\-\s]+/g', '-');
+		nh = $.trim(nh);
+		alert(nh);
+	});
 	$(".np").css("display", "none");
 	$(".nhd .nh").click(function(){
 		if($(this).parent().find('.np').css("display") == "block"){
